94576c59b8c00ae95da79a4ae132a1563908ea1b
F2077 <ubizhang@hotmail.com>
2025-07-11 18:09:02 +0800

    添加 AMF0 String 类型支持，实现 UTF-8 短字符串的序列化功能
    Signed-off-by: F2077 <ubizhang@hotmail.com>

--- src/amf0.rs
+++ src/amf0.rs
@@ -1,5 +1,6 @@
 use crate::traits::{FromBytes, ToBytes};
 use crate::type_marker::TypeMarker;
+use crate::utf8::Utf8Short;
 
 // An AMF 0 Number type is used to encode an ActionScript Number.
 // The data following a Number type marker is always an 8 byte IEEE-754 double precision floating point value in network byte order (sign bit in low memory).
@@ -98,5 +99,41 @@ impl FromBytes for BooleanType {
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
         let value = buf[1] != 0;
         Ok(Self { type_marker, value })
+    }
+}
+
+//	All strings in AMF are encoded using UTF-8; however, the byte-length header format
+//	may vary. The AMF 0 String type uses the standard byte-length header (i.e. U16). For
+//	long Strings that require more than 65535 bytes to encode in UTF-8, the AMF 0 Long
+//	String type should be used.
+pub struct StringType<'a> {
+    type_marker: TypeMarker,
+    value: Utf8Short<'a>,
+}
+
+impl<'a> ToBytes for StringType<'a> {
+    fn to_bytes(&self) -> std::io::Result<Vec<u8>> {
+        let mut vec = Vec::with_capacity(self.bytes_size());
+        vec.push(self.type_marker as u8);
+        &self.value.write_bytes_to(&mut vec[1..])?;
+        Ok(vec)
+    }
+
+    fn bytes_size(&self) -> usize {
+        1 + self.value.bytes_size()
+    }
+
+    fn write_bytes_to(&self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let required_size = 1 + self.value.bytes_size();
+        if buf.len() < required_size {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Buffer is too small, need at least {} bytes", required_size),
+            ));
+        }
+        buf[0] = self.type_marker as u8;
+        let n = &self.value.write_bytes_to(&mut buf[1..])?;
+
+        Ok(1 + n)
     }
 }
